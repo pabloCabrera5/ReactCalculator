6aa76cc9944134d9894a991ed5f4300af5d46c8c
Pablo CABRERA PEREZ <pablo.cabrera@alten.es>
2020-05-10 23:23:32 +0200

    Updates

---
+++ precache-manifest.e61b75f560249691a1db9ffcccd4a276.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85a533779ed42483173f9a79cb2d0dae",
+    "url": "/ReactCalculator/index.html"
+  },
+  {
+    "revision": "82563304c54314539510",
+    "url": "/ReactCalculator/static/css/main.d52a5222.chunk.css"
+  },
+  {
+    "revision": "dfb0571ace7b5741d0dd",
+    "url": "/ReactCalculator/static/js/2.fa47f809.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactCalculator/static/js/2.fa47f809.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "82563304c54314539510",
+    "url": "/ReactCalculator/static/js/main.34157fba.chunk.js"
+  },
+  {
+    "revision": "22a35caa3c7582105b52",
+    "url": "/ReactCalculator/static/js/runtime-main.64185f2f.js"
+  }
+]);
